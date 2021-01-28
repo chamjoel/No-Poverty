8c2f42892951a11659866bec1441545ab94ec097
Joel-Chambergo <chambergo.joel@iisgalvanimi.edu.it>
2021-01-28 19:09:26 +0000

    quasi finito

---
+++ Poverty/routes/dati.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    const uri = 'mongodb+srv://fchambergo:fchambergo@nraboy-sample.yuvim.mongodb.net/test'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+
+        const collection = client.db("no_poverty").collection("sample_poverty");
+        // perform actions on the collection object
+        collection.find().limit(10).toArray((err, result) => {
+            if (err) console.log(err.message); //Se c'è qualche errore lo stampo
+            else res.send(result);
+            client.close(); //Quando ho terminato la find chiudo la sessione con il db
+        }); //Eseguo la query e passo una funzione di callback
+
+    });
+
+});
+
+module.exports = router;
+router.get('/luogo/:location', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    location = req.params.location;
+    const uri = 'mongodb+srv://fchambergo:fchambergo@nraboy-sample.yuvim.mongodb.net/test'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(getLocation);
+    
+    function getLocation(err){
+        if (err) console.log("Connessione al db non riuscita"); 
+        else{
+            const collection = client.db("no_poverty").collection("sample_poverty");
+            collection.find({ 'GeoAreaName': `${location}` }).toArray(callBackQuery);
+        }
+    }    
+    
+    function callBackQuery(err,result){
+        if (err) console.log(err.message); 
+        else res.send(result);
+        client.close(); 
+    }  
+
+
+});
+router.get('/anno/:year', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    year = parseInt(req.params.year);
+    const uri = 'mongodb+srv://fchambergo:fchambergo@nraboy-sample.yuvim.mongodb.net/test'
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(getYear);
+
+    function getYear(err){
+        if (err) console.log("Connessione al db non riuscita"); 
+        else{
+            const collection = client.db("no_poverty").collection("sample_poverty");
+        collection.find({ 'TimePeriod': `${year}`}).toArray(callBackQuery);
+        }
+    }
+
+    function callBackQuery(err,result){
+        if (err) console.log(err.message); 
+        else res.send(result);
+        client.close(); 
+    }   
+
+    
+});
